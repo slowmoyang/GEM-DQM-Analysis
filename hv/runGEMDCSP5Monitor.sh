7418f439dfe74b3661b44e83972702c557546753
Seungjin Yang <slowmoyang@gmail.com>
2022-08-29 17:09:15 +0900

    fix a script to run GEMDCSP5Monitor.py

--- hv/runGEMDCSP5Monitor.sh
+++ hv/runGEMDCSP5Monitor.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+
 export GEM_PRODUCTION_DB_COND=
 export GEM_PRODUCTION_DB_NAME=
 
 run_2022a_start_run="2022-05-25_16:07:57"
 now=$(date +%Y-%m-%d_%H:%M:%S)
+
+echo "start: ${run_2022a_start_run}"
+echo "end: ${now}"
 
 python3 GEMDCSP5Monitor.py ${run2022a_start_run} ${now} HV 0
