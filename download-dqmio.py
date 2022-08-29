bf39ebf26f9594a50acf548c9db1fc2e2d7b9a96
Seungjin Yang <slowmoyang@gmail.com>
2022-08-29 15:57:40 +0900

    use the primary dataset name instead of DAS dataset path. Since it retrieves eras using OMS API.

--- download-dqmio.py
+++ download-dqmio.py
@@ -20,11 +20,63 @@ import ssl
 import itertools
 from bs4 import BeautifulSoup
 import tqdm
+from omsapi import OMSAPI
+import json
+from functools import cache
 
 
 CMSWEB_NETLOC = 'https://cmsweb.cern.ch/'
 
+###############################################################################
+# OMS 
+###############################################################################
+@cache
+def load_oms_api():
+    api_url = "https://cmsoms.cern.ch/agg/api"
+    api_version = "v1"
+    omsapi = OMSAPI(api_url, api_version, cert_verify=False)
+    with open('./test/oms-test/oms-api-client-auth.json', 'r') as json_file:
+        auth = json.load(json_file)
+    omsapi.auth_oidc(client_id=auth['id'], client_secret=auth['secret'])
+    return omsapi
 
+_ERA_TO_RUN_RANGE_LIST: list[tuple[str, int, int]] = []
+
+def _query_era(run: int) -> str:
+    omsapi = load_oms_api()
+    query = omsapi.query('eras')
+    filters = [
+        {
+            'attribute_name': 'start_run',
+            "value": run,
+            "operator": "LE"
+        },
+        {
+            "attribute_name": "end_run",
+            "value": run,
+            "operator": "GE"
+        }
+    ]
+    query.filters(filters)
+    data = query.data().json()
+    era: str = data['data'][0]['id']
+    start_run: int = data['data'][0]['attributes']['start_run']
+    end_run: int = data['data'][0]['attributes']['end_run']
+
+    _ERA_TO_RUN_RANGE_LIST.append((era, start_run, end_run))
+    return era
+
+
+def query_era(run: int) -> str:
+    for cached_era, start_run, end_run in _ERA_TO_RUN_RANGE_LIST:
+        if run >= start_run and run <= end_run:
+            return cached_era
+    else:
+        return _query_era(run)
+
+###############################################################################
+# CERN CERTIFICATION
+###############################################################################
 @dataclass
 class CertChain:
     certfile: str
@@ -32,9 +84,14 @@ class CertChain:
 
 
 def load_cert_chain() -> CertChain:
-    cert_dir = Path(os.getenv('CERN_CERTIFICATE_PATH', Path.home() / '.globus'))
+    if (cert_dir := os.getenv('CERN_CERTIFICATE_PATH')) is not None:
+        cert_dir = Path(cert_dir)
+    else:
+        cert_dir = Path.home() / '.globus'
+        print(f"The environment variable 'CERN_CERTIFICATE_PATH' is not set. It fallbacks to '{str(cert_dir)}'")
     if not cert_dir.exists():
         raise FileNotFoundError(cert_dir)
+
     certfile = cert_dir / 'usercert.pem'
     if not certfile.exists():
         raise FileNotFoundError(certfile)
@@ -67,91 +124,63 @@ class HTTPSAuthHandler(urllib.request.AbstractHTTPHandler):
         return self.do_open(http_class=HTTPSAuthConnection, # type: ignore
                             req=req)
 
-@dataclass
-class DQMDatasetInfo:
-    """https://twiki.cern.ch/twiki/bin/view/CMSPublic/CompOpsTier0Policies#Dataset_Naming_Conventions_for_T"""
-    primary: str
-    acquisition_era: str
-    stream: str
-    proc_version: str
-    tier: str
-
-    @classmethod
-    def from_das_name(cls, dataset) -> DQMDatasetInfo:
-        # /[ExpressDataset]/[AcquisitionEra]-Express-[ExpressProcVersion]/DQM
-        # /[PrimaryDataset]/[AcquisitionEra]-PromptReco-[RecoProcVersion]/DQM
-        _, primary, processed, tier = dataset.split('/')
-        acquisition_era, stream, proc_version = processed.split('-')
-        instance = cls(primary, acquisition_era, stream, proc_version, tier)
-        if dataset != str(instance):
-            raise RuntimeError(f"failed to parse dataset: {dataset} ==> {str(instance)}") # TODO URLNotFoundError
-        return instance
-
-    @property
-    def processed(self) -> str:
-        return f'{self.acquisition_era}-{self.stream}-{self.proc_version}'
-
-    def __str__(self) -> str:
-        return f'/{self.primary}/{self.processed}/{self.tier}'
-
+###############################################################################
+# DQM
+###############################################################################
 
 class LinkFinderCallable(Protocol):
     def __call__(self, run: int, dataset: str) -> str:
         ...
 
-def find_offline_dqm_file_link(dataset: str, run: int) -> str:
-    info = DQMDatasetInfo.from_das_name(dataset)
-    run_str = f'{run:0>9d}'
 
-    # FIXME incorrect terms...
-    era = info.acquisition_era
-    if re.match(r'Run\d{4}[A-Z]', era):
-        era = era[:-1]
-
-    url = f"dqm/offline/data/browse/ROOT/OfflineData/{era}/{info.primary}/{run_str[:-2]}xx/"
-    url = urllib.parse.urljoin(CMSWEB_NETLOC, url)
-
-    filename_pattern = re.compile(rf'DQM_V\d{{4}}_R{run_str}__{info.primary}__{info.processed}__{info.tier}.root')
-
-    response = urllib.request.build_opener(HTTPSAuthHandler()).open(url)
+def _find_file_url(url: str, pattern: re.Pattern) -> str:
+    try:
+        response = urllib.request.build_opener(HTTPSAuthHandler()).open(url)
+    except Exception as error:
+        print(f'got exception: {url=:s}')
+        raise error
+    if response.status != HTTPStatus.OK:
+        raise RuntimeError(f'[{response.status=}] {url=:s}')
 
     soup = BeautifulSoup(response, features="html.parser")
     for a_tag in soup.find_all('a'):
         href = a_tag.attrs['href']
         last = href.split('/')[-1]
-        if re.match(filename_pattern, last):
+        if re.match(pattern, last):
             return urllib.parse.urljoin(CMSWEB_NETLOC, href)
     else:
-        raise RuntimeError(f"failed to find a filename matched with '{filename_pattern}' from '{url}'") # TODO raise proper error
+        raise RuntimeError(f"failed to find a filename matched with '{pattern}' from '{url}'") # TODO raise proper error
 
 
-def find_online_dqm_file_link(run: int, client: str) -> str:
+
+def find_offline_dqm_file_link(dataset: str, run: int) -> str:
+    r"""
+    :dataset: primary dataset
+    :run: run number
+    :returns:
+    """
+    run_str = f'{run:0>9d}'
+    era = query_era(run=run)
+    if re.match(r'Run\d{4}[A-Z]', era):
+        era = era[:-1]
+
+    url = f"dqm/offline/data/browse/ROOT/OfflineData/{era}/{dataset}/{run_str[:-2]}xx/"
+    url = urllib.parse.urljoin(CMSWEB_NETLOC, url)
+
+    # filename pattern
+    pattern = re.compile(rf'DQM_V\d{{4}}_R{run_str}__{dataset}__[a-zA-Z0-9\-]+__DQMIO.root')
+    return _find_file_url(url=url, pattern=pattern)
+
+
+def find_online_dqm_file_link(run: int, dataset: str) -> str:
     r'''
     https://cmsweb.cern.ch/dqm/offline/data/browse/ROOT/OnlineData/original/00035xxxx/0003553xx/DQM_V0001_GEM_R000355361.root
     '''
     run_str = str(run)
-    index_url = f"dqm/offline/data/browse/ROOT/OnlineData/original/{run_str[:-4]:0>5}xxxx/{run_str[:-2]:0>7}xx"
-    index_url = urllib.parse.urljoin(CMSWEB_NETLOC, index_url)
-    filename_pattern = re.compile(rf'DQM_V\d{{4}}_{client:s}_R{run:0>9}.root')
-
-    try:
-        response = urllib.request.build_opener(HTTPSAuthHandler()).open(index_url)
-    except Exception as error:
-        print(f'got an error while opening {index_url=}')
-        raise error
-
-    soup = BeautifulSoup(response, features="html.parser")
-    for row in soup.find_all('tr'):
-        # link, file size, date
-        link, _, _ = row.find_all('td')
-        filename = link.text
-        if filename_pattern.match(filename):
-            url = link.find('a').attrs['href']
-            url = urllib.parse.urljoin(CMSWEB_NETLOC, url)
-            return url
-    else:
-        raise RuntimeError(f'{run=} not found on {index_url}')
-
+    url = f"dqm/offline/data/browse/ROOT/OnlineData/original/{run_str[:-4]:0>5}xxxx/{run_str[:-2]:0>7}xx"
+    url = urllib.parse.urljoin(CMSWEB_NETLOC, url)
+    pattern = re.compile(rf'DQM_V\d{{4}}_{dataset:s}_R{run:0>9}.root')
+    return _find_file_url(url=url, pattern=pattern)
 
 def download_root_file(url: str,
                        filename: Optional[str] = None,
@@ -221,28 +250,22 @@ def main():
     subparsers = parser.add_subparsers(required=True)
 
     # Offline DQM
-    offline_parser = subparsers.add_parser('offline', aliases=['off'],
-                                           help='download offline dqm files')
-    offline_parser.add_argument("-r", "--run", type=str,
-                                nargs="+", required=True, help="runs")
-    offline_parser.add_argument("-d", "--dataset",
-                                type=str, nargs="+", required=True,
-                                help="datasets, DAS convention")
+    offline_parser = subparsers.add_parser('offline', aliases=['off'], help='download offline dqm files')
+    offline_parser.add_argument("-r", "--run", type=str, nargs="+", required=True, help="runs")
+    offline_parser.add_argument("-d", "--dataset", type=str, nargs="+", required=True,
+                                help="primary datasets like 'StreamExpress', 'SingleMuon', 'DoubleMuon', and 'Muon'.")
     offline_parser.set_defaults(link_finder=find_offline_dqm_file_link)
 
     # Online DQM
-    online_parser = subparsers.add_parser('online', aliases=['on'],
-                                          help='download online dqm files')
-    online_parser.add_argument("-r", "--run", type=str, nargs="+", required=True,
-                               help="runs")
-    online_parser.add_argument('-c', '--client', dest='dataset',
-                               type=str, nargs='+',
-                               default=['GEM'],
+    online_parser = subparsers.add_parser('online', aliases=['on'], help='download online dqm files')
+    online_parser.add_argument("-r", "--run", type=str, nargs="+", required=True, help="runs")
+    online_parser.add_argument('-d', '--dataset', type=str, nargs='+', default=['GEM'],
                                help='online dqm clients (https://github.com/cms-sw/cmssw/tree/master/DQM/Integration/python/clients)')
     online_parser.set_defaults(link_finder=find_online_dqm_file_link)
 
     # common
-    parser.add_argument('-o', '--output-dir', type=Path, help='output directory')
+    for each in [offline_parser, online_parser]:
+        each.add_argument('-o', '--output-dir', type=Path, help='output directory')
 
     # parse
     args = parser.parse_args()
